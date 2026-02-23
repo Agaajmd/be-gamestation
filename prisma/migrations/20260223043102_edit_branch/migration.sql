1d4dd4e770ce4c2f9976b9eac95f7f192e8d00c9
eggimugi <eggimugi03@gmail.com>
2026-02-23 11:36:34 +0700

    fix: update time format in migration script for branches table

--- prisma/migrations/20260223043102_edit_branch/migration.sql
+++ prisma/migrations/20260223043102_edit_branch/migration.sql
@@ -1,13 +1,11 @@
--- Fill null values first
 UPDATE "branches" SET "address" = '' WHERE "address" IS NULL;
 UPDATE "branches" SET "phone" = '' WHERE "phone" IS NULL;
-UPDATE "branches" SET "open_time" = '09:00' WHERE "open_time" IS NULL;
-UPDATE "branches" SET "close_time" = '22:00' WHERE "close_time" IS NULL;
+UPDATE "branches" SET "open_time" = '09:00:00' WHERE "open_time" IS NULL;
+UPDATE "branches" SET "close_time" = '22:00:00' WHERE "close_time" IS NULL;
 
--- AlterTable
 ALTER TABLE "branches" ALTER COLUMN "address" SET NOT NULL,
 ALTER COLUMN "phone" SET NOT NULL,
-ALTER COLUMN "open_time" TYPE VARCHAR(5),
+ALTER COLUMN "open_time" TYPE VARCHAR(8),
 ALTER COLUMN "open_time" SET NOT NULL,
-ALTER COLUMN "close_time" TYPE VARCHAR(5),
+ALTER COLUMN "close_time" TYPE VARCHAR(8),
 ALTER COLUMN "close_time" SET NOT NULL;
